e9a81df5ef9234fd8b502f9c0c92294819ded6ab
Lee0701 <hgl1002@naver.com>
2023-11-24 16:48:46 +0900

    Add population conversion

--- generate_both_names.js
+++ generate_both_names.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs')
 const readline = require('readline')
-const alternateNamesDict = require('./alternateNames/alternateNames_zh_hant_dict.json')
+const alternateNamesDict = require('./data/alternateNames/alternateNames_zh_hant_dict.json')
 
 const north = 60.00958
 const east = 20.00958
@@ -10,16 +10,16 @@ const west = -10.00958
 const header = [north, east, south, west].join(',')
 
 const THRES_COUNT = 120000
-// const THRES_CITY = Math.sqrt(500000)
-// const THRES_L = Math.sqrt(250000)
-// const THRES_M = Math.sqrt(100000)
 const THRES_CITY = 500000
 const THRES_L = 100000
 const THRES_M = 75000
 
+const convertPopulation = (population) => Math.round(Math.sqrt(population) * 2)
+// const convertPopulation = (population) => Math.round(population / 100)
+
 const main = async () => {
-    const inFile = 'allCountries.txt'
-    const outFile = 'EU_edit_both_names.csv'
+    const inFile = 'data/cities1000.txt'
+    const outFile = 'data/EUXXL_both_names.csv'
     const result = []
     const failed = []
     const count = {S: 0, M: 0, L: 0, C: 0}
@@ -49,13 +49,14 @@ const main = async () => {
             failed.push(line)
             return
         }
-        const outPopulation = parseInt(population)
+        const inPopulation = Math.round(population)
+        const outPopulation = convertPopulation(population)
         const normalizedName = outName.replace(/,/g, ' ').replace(/ +/, ' ').trim()
-        const outSize = (outPopulation > THRES_L) ? 'L' : (outPopulation > THRES_M) ? 'M' : 'S'
-        const outCity = (outPopulation > THRES_CITY) ? '1' : '0'
+        const outSize = (inPopulation > THRES_L) ? 'L' : (inPopulation > THRES_M) ? 'M' : 'S'
+        const outCity = (inPopulation > THRES_CITY) ? '1' : '0'
         count[outSize] += 1
         if(outCity == '1') count['C'] += 1
-        result.push([outPopulation, normalizedName, outSize, outCity, outLatitude, outLongitude])
+        result.push([inPopulation, normalizedName, outPopulation, outCity, outLatitude, outLongitude])
         if(++i % 10000 === 0) console.log(i)
     })
     rl.on('close', () => {
@@ -65,7 +66,7 @@ const main = async () => {
                 .slice(0, THRES_COUNT)
                 .map((items) => items.slice(1).join(','))
         fs.writeFileSync(outFile, header + '\n' + output.join('\n'))
-        fs.writeFileSync('failed.tsv', failed.join('\n'))
+        fs.writeFileSync('data/failed.tsv', failed.join('\n'))
     })
 }
 
